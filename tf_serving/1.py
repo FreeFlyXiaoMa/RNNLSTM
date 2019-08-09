2350d085533970de0f56f9506943bb08325fb5db
FreeFlyXiaoMa <2429825530@qq.com>
2019-08-09 15:51:45 +0800

    demo1、2、3---宽、深度模型练习

--- tf_serving/1.py
+++ tf_serving/1.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 #@Time    :2019/8/6 14:40
 #@Author  :XiaoMa
+from Cython.Compiler.ExprNodes import ListNode
 
 a=10
-def global_test():
-    global a
-    a=2018
-    print('global_test:a=%d'%(a))
-global_test()
-print('a==',a)
+# def global_test():
+#     global a
+#     a=2018
+#     print('global_test:a=%d'%(a))
+# global_test()
+# print('a==',a)
+ListNode()
